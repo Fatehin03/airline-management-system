969b3c36f4c9d4aa26433ccec1e20042b71de43f
Fatehin03 <fatehinalam03@gmail.com>
2026-01-30 19:39:18 +0600

    Create database.py

---
+++ airline-backend/app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.settings import settings
+
+# Render uses "postgres://" but SQLAlchemy needs "postgresql://"
+DB_URL = settings.DATABASE_URL.replace("postgres://", "postgresql://")
+
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
